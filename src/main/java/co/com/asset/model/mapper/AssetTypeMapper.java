0c918e8432834affafa8bfc0d00b81d6bc11efad
Edwin Acosta Bravo <edalac2003@yahoo.com.ar>
2023-11-13 19:35:59 -0500

    Code cleaning 4

--- src/main/java/co/com/asset/model/mapper/AssetTypeMapper.java
+++ src/main/java/co/com/asset/model/mapper/AssetTypeMapper.java
@@ -1,7 +1,6 @@
 package co.com.asset.model.mapper;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 
--- src/main/java/co/com/asset/service/asset/impl/AssetServiceImpl.java
+++ src/main/java/co/com/asset/service/asset/impl/AssetServiceImpl.java
@@ -3,7 +3,6 @@ package co.com.asset.service.asset.impl;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Propagation;
